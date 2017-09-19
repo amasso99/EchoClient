49afb8c6a1d051bce97f4b3af3ad98197f48b2d7
Oussama <khouri.oussama@gmx.de>
2017-09-20 00:22:25 +0200

    HOMEWORK

--- src/Control/Networkclasses/EchoClient.java
+++ src/Control/Networkclasses/EchoClient.java
@@ -3,7 +3,15 @@ package Control.Networkclasses;
 /**
  * Created by Jean-Pierre on 18.09.2017.
  */
-public class EchoClient {
+public class EchoClient extends Client{
 
     //TODO Diese Klasse muss noch vollständig geschrieben werden.
+    public EchoClient(String pServerIP, int pServerPort) {
+        super(pServerIP, pServerPort);
+    }
+
+    @Override
+    public void processMessage(String pMessage) {
+
+    }
 }
--- src/Control/Networkclasses/EchoServer.java
+++ src/Control/Networkclasses/EchoServer.java
@@ -3,7 +3,24 @@ package Control.Networkclasses;
 /**
  * Created by Jean-Pierre on 18.09.2017.
  */
-public class EchoServer {
-
+public class EchoServer extends Server{
     //TODO Diese Klasse muss noch vollständig geschrieben werden.
+    public EchoServer(int pPort) {
+        super(pPort);
+    }
+
+    @Override
+    public void processNewConnection(String pClientIP, int pClientPort) {
+
+    }
+
+    @Override
+    public void processMessage(String pClientIP, int pClientPort, String pMessage) {
+
+    }
+
+    @Override
+    public void processClosingConnection(String pClientIP, int pClientPort) {
+
+    }
 }
